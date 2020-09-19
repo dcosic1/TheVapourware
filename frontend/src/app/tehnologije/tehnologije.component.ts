6c6ae348202df7aed97a97481a5ce92ab60cd4ad
Adna Tucak <tucakadna@gmail.com>
2020-09-19 15:06:42 +0200

    Fixes

--- frontend/src/app/tehnologije/tehnologije.component.ts
+++ frontend/src/app/tehnologije/tehnologije.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { TehnologijeService } from "./tehnologije.service";
 import { Tehnologije } from "../models/tehnologije.model";
 import { ToastrService } from "ngx-toastr";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-tehnologije",
@@ -10,10 +11,13 @@ import { ToastrService } from "ngx-toastr";
 })
 export class TehnologijeComponent implements OnInit {
   tehnologije: Tehnologije;
+  tehnologijeForm: FormGroup;
+  submitted = false;
 
   constructor(
     private tehnologijeService: TehnologijeService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder
   ) {}
 
   showToaster() {
@@ -24,5 +28,26 @@ export class TehnologijeComponent implements OnInit {
     this.tehnologijeService.getTehnologije().subscribe(Tehnologije => {
       this.tehnologije = Tehnologije;
     });
+
+    this.tehnologijeForm = this.formBuilder.group({
+      projekat: ["", Validators.required],
+      trenutnaTehnologija: ["", Validators.required],
+      novaTehnologija: ["", [Validators.required, Validators.email]],
+      potrebnaZnanja: ["", Validators.required],
+      razlog: ["", Validators.required],
+      nadlezna: ["", Validators.required]
+    });
+  }
+  get f() {
+    return this.tehnologijeForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.tehnologijeForm.invalid) {
+      console.log("tehnoooo")
+      return;
+    }
+    this.showToaster();
   }
 }
